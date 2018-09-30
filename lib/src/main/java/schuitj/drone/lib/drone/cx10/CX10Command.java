f2446095bd5c81cbc289823b3cf7a9fa90f793c1
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-09-30 13:25:12 +0200

    cleanup

--- lib/src/main/java/schuitj/drone/lib/drone/cx10/CX10Command.java
+++ lib/src/main/java/schuitj/drone/lib/drone/cx10/CX10Command.java
@@ -15,28 +15,27 @@ public class CX10Command implements Command {
 
     @Override
     public byte[] toByteArray() {
-        int pitch = this.getPitch() + 128;
-        int yaw = this.getYaw() + 128;
-        int roll = this.getRoll() + 128;
-        int throttle = this.getThrottle() + 128;
+        byte pitch    = (byte) (this.getPitch() + 128);
+        byte yaw      = (byte) (this.getYaw() + 128);
+        byte roll     = (byte) (this.getRoll() + 128);
+        byte throttle = (byte) (this.getThrottle() + 128);
         boolean takeOff = this.isTakeOff();
         boolean land = this.isLand();
 
         byte[] data = new byte[8];
         data[0] = (byte) 0xCC;
-        data[1] = (byte) roll;
-        data[2] = (byte) pitch;
-        data[3] = (byte) throttle;
-        data[4] = (byte) yaw;
+        data[1] = roll;
+        data[2] = pitch;
+        data[3] = throttle;
+        data[4] = yaw;
         data[5] = takeOff ? (byte) 0x01 : (land ? (byte) 0x02 : 0);
-
         data[6] = createChecksum(ByteUtils.asUnsigned(data[1], data[2], data[3], data[4], data[5]));
-
         data[7] = (byte) 0x33;
+
         return data;
     }
 
-    public static final byte createChecksum(byte[] bytes) {
+    private byte createChecksum(byte[] bytes) {
         byte sum = 0;
         for (byte b : bytes) {
             sum ^= b;
